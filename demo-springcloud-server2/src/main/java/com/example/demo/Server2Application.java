eeb6f50be01b5e6b73fa10c7f467d8f1ce1a00ab
yanghq <yhq7389106>
2017-08-31 09:58:06 +0800

    demo

--- demo-springcloud-server2/src/main/java/com/example/demo/Server2Application.java
+++ demo-springcloud-server2/src/main/java/com/example/demo/Server2Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableEurekaServer
 public class Server2Application {
 
