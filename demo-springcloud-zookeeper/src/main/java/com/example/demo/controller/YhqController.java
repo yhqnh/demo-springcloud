0e75c62622af870b2e3e3d50b6fdf9fea1bf17d2
yanghq <yhq7389106>
2017-09-22 18:36:24 +0800

    demo

--- demo-springcloud-zookeeper/src/main/java/com/example/demo/controller/YhqController.java
+++ demo-springcloud-zookeeper/src/main/java/com/example/demo/controller/YhqController.java
@@ -1,14 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.vo.BaseResponse;
-import com.example.demo.vo.People;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author yhq
@@ -17,18 +9,5 @@ import java.util.List;
 @RestController
 public class YhqController {
 
-    @RequestMapping("/swagger")
-    @ApiOperation(value = "value")
-    public BaseResponse<List<People>> swagger(@RequestParam("prop") String prop){
-        BaseResponse baseResponse = new BaseResponse();
-        baseResponse.setCode(200);
-        baseResponse.setMessage("test");
-        List list = new ArrayList();
-        People people = new People();
-        people.setName("people");
-        people.setSex("nan");
-        list.add(people);
-        baseResponse.setData(list);
-        return baseResponse;
-    }
+
 }
